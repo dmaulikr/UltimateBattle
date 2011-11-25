d4ee334d823a54496fe8dfd1343a9b5aaf17a67a
Anthony Broussard <anthony@chaione.com>
2011-11-25 16:39:58 -0600

    Cocos added and all tests passing.

--- clonepilot/Clone Pilot/Clone Pilot/Clone_PilotAppDelegate.m
+++ clonepilot/Clone Pilot/Clone Pilot/Clone_PilotAppDelegate.m
@@ -6,9 +6,13 @@
 //  Copyright 2011 ChaiONE. All rights reserved.
 //
 
+#import "cocos2d.h"
+
 #import "Clone_PilotAppDelegate.h"
 
 #import "Clone_PilotViewController.h"
+
+
 
 @implementation Clone_PilotAppDelegate
 
@@ -17,53 +21,92 @@
 
 @synthesize viewController=_viewController;
 
+#pragma mark cocos2d
+
+- (void) removeStartupFlicker {
+    
+}
+
+#pragma mark standard
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-     
+    
     self.window.rootViewController = self.viewController;
+    
+    if( ! [CCDirector setDirectorType:kCCDirectorTypeDisplayLink] ) {
+		[CCDirector setDirectorType:kCCDirectorTypeDefault];
+    }
+    
+ 	CCDirector *director = [CCDirector sharedDirector];
+    self.viewController.wantsFullScreenLayout = YES;
+    
+    EAGLView *glView = [EAGLView viewWithFrame:[self.window bounds]
+								   pixelFormat:kEAGLColorFormatRGB565	// kEAGLColorFormatRGBA8
+								   depthFormat:0						// GL_DEPTH_COMPONENT16_OES
+						];
+	
+	// attach the openglView to the director
+	[director setOpenGLView:glView];
+    
+	[director setDeviceOrientation:kCCDeviceOrientationPortrait];
+    
+    [director setAnimationInterval:1.0/60];
+	[director setDisplayFPS:YES];
+    [self.viewController setView:glView];
+    
+    // make the View Controller a child of the main window
+	//[window addSubview: viewController.view];
+	
     [self.window makeKeyAndVisible];
+    
+    [CCTexture2D setDefaultAlphaPixelFormat:kCCTexture2DPixelFormat_RGBA8888];
+    
+    //	[[CCDirector sharedDirector] runWithScene: [HelloWorldLayer scene]];    
+    
+    
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+#pragma application delegate methods with ccdirector
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+	[[CCDirector sharedDirector] pause];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+	[[CCDirector sharedDirector] resume];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+	[[CCDirector sharedDirector] purgeCachedData];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+-(void) applicationDidEnterBackground:(UIApplication*)application {
+	[[CCDirector sharedDirector] stopAnimation];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+-(void) applicationWillEnterForeground:(UIApplication*)application {
+	[[CCDirector sharedDirector] startAnimation];
 }
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+	CCDirector *director = [CCDirector sharedDirector];
+	
+	[[director openGLView] removeFromSuperview];
+    
+    self.viewController = nil;
+    self.window = nil;
+	
+	[director end];	
+}
+
+- (void)applicationSignificantTimeChange:(UIApplication *)application {
+	[[CCDirector sharedDirector] setNextDeltaTimeZero:YES];
+}
+
 
 - (void)dealloc
 {
