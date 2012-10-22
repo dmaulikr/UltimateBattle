351f13991721fc3706c17cd5e5be5170f0e272ca
Anthony Broussard <anthony.broussard@chaione.com>
2012-10-22 18:13:36 -0500

    Pilot ready for building.

--- QuantumPilot/QuantumPilot/QuantumPilot.h
+++ QuantumPilot/QuantumPilot/QuantumPilot.h
@@ -8,23 +8,9 @@
 
 #import "CCNode.h"
 
-struct course {
-    CGPoint const[4001];
-    BOOL firing[4001];`
-};
-
-struct array {
-    int array[10][10];
-};
-
 @interface QuantumPilot : CCNode {
-    struct course _nav;
+
 }
 
-@property (nonatomic) struct course n;
-
-+(struct array)returnArray2;
-- (struct course)navigationCourse;
-- (void)setWaypoint:(CGPoint)l index:(NSInteger)index;
 
 @end
--- QuantumPilot/QuantumPilot/QuantumPilot.m
+++ QuantumPilot/QuantumPilot/QuantumPilot.m
@@ -9,28 +9,5 @@
 #import "QuantumPilot.h"
 
 @implementation QuantumPilot
-@synthesize n;
-
-+(int (*)[10][10])returnArray {
-    int (*array)[10][10] = malloc(10 * 10 * sizeof(int));
-    return array;
-}
-
-// return a ructure
-
-+(struct array)returnArray2 {
-    struct array a;
-    a.array[2][9] = 5;
-    return a;
-}
-
-- (void)setWaypoint:(CGPoint)l index:(NSInteger)index {
-    self.n.waypoints[index] = l;
-    _nav.waypoints[index] = l;
-}
-
-- (struct course)navigationCourse {
-    return _nav;
-}
 
 @end
