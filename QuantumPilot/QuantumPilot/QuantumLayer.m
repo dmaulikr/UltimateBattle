a19ddde1f14fae159456b0ac199aab67f0976a15
Anthony Broussard <anthony.broussard@chaione.com>
2012-10-23 00:29:52 -0500

    yDirection matches pulse drawing

--- QuantumPilot/QuantumPilot/QuantumLayer.m
+++ QuantumPilot/QuantumPilot/QuantumLayer.m
@@ -32,14 +32,14 @@
         TriLaser *tl = [[TriLaser alloc] init];
         tl.yDirection = -1;
         tl.l = ccp(500, 400);
-        tl.vel = ccp(0,1);
+        tl.vel = ccp(0,tl.yDirection);
         [self.f addChild:tl];
         [self.f.bullets addObject:tl];
 
         TriLaser *tl2 = [[TriLaser alloc] init];
         tl2.yDirection = 1;
         tl2.l = ccp(600, 400);
-        tl2.vel = ccp(0,-1);
+        tl2.vel = ccp(0,tl2.yDirection);
         [self.f addChild:tl2];
         [self.f.bullets addObject:tl2];
         
