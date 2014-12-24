4e354608b91071d51bee3b357c8ba08ede50ae66
quantumpotato <quantumpotato@gmail.com>
2014-12-23 19:28:37 -0500

    Exact debris collection

--- QuantumPilot/QuantumPilot/QuantumPilot.m
+++ QuantumPilot/QuantumPilot/QuantumPilot.m
@@ -292,15 +292,7 @@ static float innerTopHeight = 5.75;
 }
 
 - (BOOL)isCollidingWithDebris:(Debris *)d {
-    float distance = GetDistance(self.l, d.l);
-    if (distance < 20) {
-        float mvDist = GetDistance(CombinedPoint(self.l, self.vel), d.l);
-        if (mvDist <= distance) {
-            return YES;
-        }
-    }
-    
-    return NO;
+    return shapeOfSizeContainsPoint(outerEdges, 4, d.l);
 }
 
 - (void)registerHit {
